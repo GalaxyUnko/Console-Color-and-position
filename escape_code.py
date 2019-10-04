6d1ec493c6d3c65b0a28be1937e37927185f587a
Hikaru Takahashi <mitsuru11241112213@gmail.com>
2019-10-04 18:02:22 +0900

    Update escape_code.py

--- escape_code.py
+++ escape_code.py
@@ -10,7 +10,7 @@ print ('%s, %s' % (s1, s2))  # Hello, worldと表示
 
 
 # Terminalの色とか表示位置とか
-# https://qiita.com/PruneMazui/items/8a023347772620025ad6
+# シェルスクリプト　→　https://qiita.com/PruneMazui/items/8a023347772620025ad6
 # https://www.mm2d.net/main/prog/c/console-02.html
 
 x = 1
